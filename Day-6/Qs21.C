79f36ecd35d32ce2293bf87c8caff13d5aed939f
Muhammad Faizan <faizan967044@gmail.com>
2026-06-06 09:49:35 +0530

    Added Day-6 C programs

---
+++ Day-6/Qs21.C
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int n, binary[32], i = 0;
+
+    printf("Enter a decimal number: ");
+    scanf("%d", &n);
+
+    // Convert decimal to binary
+    while (n > 0) {
+        binary[i] = n % 2;
+        n = n / 2;
+        i++;
+    }
+
+    // Print binary number in reverse order
+    printf("Binary equivalent: ");
+    for (int j = i - 1; j >= 0; j--) {
+        printf("%d", binary[j]);
+    }
+
+    return 0;
+}
