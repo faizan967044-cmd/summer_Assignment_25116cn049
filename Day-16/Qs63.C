e40e06029e05dc4402120e6513d9b1649d28fbf1
Muhammad Faizan <faizan967044@gmail.com>
2026-06-16 09:00:50 +0530

    Day-16 assignment uploaded

---
+++ Day-16/Qs63.C
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+    int arr[100], n, sum, i, j, found = 0;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter array elements:\n");
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter the required sum: ");
+    scanf("%d", &sum);
+
+    for(i = 0; i < n; i++) {
+        for(j = i + 1; j < n; j++) {
+            if(arr[i] + arr[j] == sum) {
+                printf("Pair found: %d + %d = %d\n", arr[i], arr[j], sum);
+                found = 1;
+            }
+        }
+    }
+
+    if(found == 0) {
+        printf("No pair found with sum %d\n", sum);
+    }
+
+    return 0;
+}
